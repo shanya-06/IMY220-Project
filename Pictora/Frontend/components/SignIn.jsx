4f7afcb79b916cbec07ff26036eb75e8d722ca12
shanya-06 <u2506845@tuks.co.za>
2026-09-03 16:27:21 +0200

    added to signin component

--- Pictora/Frontend/components/SignIn.jsx
+++ Pictora/Frontend/components/SignIn.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
+
 function SignIn() {
+
+    const [formdata, setFormdata] = useState({
+        username: "",
+        password: "",
+    });
+
+
+
     return(
         <form action="">
             <h3>Log-In</h3>
             <hr/>
             <br/>
             
-            <label for="username">Username</label><br/>
-            <input type="text" id="username" required="required"></input><br/><br/>
+            <label htmlFor="username">Username</label><br/>
+            <input type="text" id="username" required value={formData.username}></input><br/><br/>
 
-            <label for="password">Password</label><br/>
-            <input type="password" id="password" required="required"></input><br/><br/>
+            <label htmlFor="password">Password</label><br/>
+            <input type="password" id="password" required value={formData.password}></input><br/><br/>
+
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            {success && <p style={{ color: "green" }}>{success}</p>}
 
             <button type="submit">Submit</button>
         </form>
